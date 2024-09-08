609c82fa87ee220835476bbdec943ac8bedf2e53
DaegyuJeong <k5946k@gmail.com>
2024-09-08 22:52:36 +0900

    test

---
+++ pages/01_DocumentGPT.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="DocumentGPT",
+    page_icon="❓",
+)
+st.title("DocumentGPT")
+
+st.markdown(
+    """
+Welcome to DocumentGPT.
+            
+I will answer your questions based on the document you provide.
+"""
+)        
